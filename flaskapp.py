fd7464a9dc80cb351c6e310af8bd1ed7d5beb151
GoldKing <GalaGoldking11>
2023-11-21 06:25:07 +0600

    Added CRUD, new routes, new pages, flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app =  Flask(__name__)
+db = SQLAlchemy()
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+'''with app.app_context():
+    db.init_app(app)'''
+
+
+app.config['SECRET_KEY'] = 'SECRETNIY KEIY'
